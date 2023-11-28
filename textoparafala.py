00e4a78b94a5e316ed34635d6101e2c80680fa5c
Maria Nascimento <marianascimentomn14@gmail.com>
2023-11-28 11:14:58 -0300

    Traduz texto digitado para audio

--- textoparafala.py
+++ textoparafala.py
@@ -1,2 +1,14 @@
 from gtts import gTTS
+import os
+from playsound import playsound
 
+texto = input("Digite aqui o texto: ")
+lingua = "pt"
+
+tts = gTTS(texto, lang=lingua)
+tts.save("audio.mp3")
+
+playsound("audio.mp3")
+
+# Remova o arquivo de áudio (opcional)
+os.remove("audio.mp3")
